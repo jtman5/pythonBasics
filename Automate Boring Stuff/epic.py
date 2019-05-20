47fe093c6e2b9ed5a0fa80730bd8bb19169f5b6b
JT Adams <35778748+jtman5@users.noreply.github.com>
2019-05-20 12:16:05 -0500

    First Ever Thingy Very Epic

---
+++ Automate Boring Stuff/epic.py
@@ -0,0 +1,11 @@
+# First ever program very epic
+
+print('Hello Gamers')
+print('How epic are you on a scale of 1-10')
+epicness = input()
+
+if epicness >= 7:
+    print('You are epic')
+else:
+    print('You arent epic')
+        
